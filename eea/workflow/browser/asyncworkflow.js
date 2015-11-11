33302e1bffcc8acb1208d3e47205e1a320ef2e20
ichim-david <ichim.david@gmail.com>
2015-11-11 13:14:15 +0200

    * Change: cleaned asyncworkflow.js and added a small feature where
      the contentMenuContent is now hidden when the async placeholder images
      is in place of the contentMenu
      [ichimdav refs #30066]

--- eea/workflow/browser/asyncworkflow.js
+++ eea/workflow/browser/asyncworkflow.js
@@ -1,54 +1,73 @@
-function AsyncWorkflow(){
+/* EXTERNAL DEPENDENCIES:
+ * plone_javascript_variables.js context_url
+ * dropdown.js initializeMenus
+ *  */
+
+function AsyncWorkflow() {
     this.handler = "@@workflow_menu";
+    this.ajaxhandler = window.context_url + '/' + this.handler;
     this.menu = jQuery("#plone-contentmenu-workflow");
     this.menuHeader = jQuery(".actionMenuHeader", this.menu);
     this.submenu = jQuery(".actionMenuContent", this.menu);
-    var self = this;
-    jQuery("#edit-bar").on('click',
-                           "#plone-contentmenu-workflow dd a",
-                           function(e){return self.handle_click(e);});
+    this.actionsMenu = jQuery(".contentActions");
+    this.loadingImg = "<img src='" + window.context_url + "/eea-ajax-loader.gif' " +
+        "alt='Changing state ...' title='Changing state ...' />";
 }
 
 AsyncWorkflow.Events = {};
 AsyncWorkflow.Events.WORKFLOW_MENU_REFRESHED = "ID-WORKFLOW_MENU_REFRESHED";
 
-AsyncWorkflow.prototype.handle_click = function(e){
+AsyncWorkflow.prototype.initialize = function() {
+    var self = this;
+    this.submenu.find('a').click(function(e) {
+        return self.handle_click(e);
+    });
+};
+
+AsyncWorkflow.prototype.handle_click = function(e) {
 
     // we're not sure who gets to be event target, we need the <a> link
-    var $element;
+    var $element = jQuery(e.target);
     if (e.target.nodeName !== 'A') {
-        $element = jQuery(e.target).parent('a');
-    } else {
-        $element = jQuery(e.target);
+        $element = $element.parent('a');
     }
 
     // compatibility with publishDialog and any other script that wants
     // to handle workflow action by itself
-    if ($element.hasClass('kssIgnore') === true){ return true; }
+    if ($element.hasClass('kssIgnore') === true) {
+        return true;
+    }
 
     var url = $element.attr('href');
-    jQuery("#plone-contentmenu-workflow .actionMenuHeader").html("<img src='" + context_url + "/eea-ajax-loader.gif' " +
-                                 "alt='Changing state ...' title='Changing state ...' />");
+    this.submenu.hide();
+    this.menuHeader.html(this.loadingImg);
     this.execute(url);
-    /* debugger; */
     return false;
 };
 
-AsyncWorkflow.prototype.execute = function(url){
+AsyncWorkflow.prototype.execute = function(url) {
     var self = this;
-    var ajaxhandler = context_url + '/' + this.handler;
-    var jxhr = jQuery.post(ajaxhandler, data={'action_url':url}, success=function(data){
-        var dom = jQuery(data);
-        jQuery(".contentActions").html(data);
-        var messages = jQuery(".contentActions .portalMessage").detach();
-        jQuery("#kssPortalMessage").after(messages);
-        jQuery(initializeMenus);    //we need to reinitialize menus
-        jQuery(AsyncWorkflow.Events).trigger(AsyncWorkflow.Events.WORKFLOW_MENU_REFRESHED);
-    }).fail(function(){
-        self.menuHeader.html("Failure!");
-    });
+    jQuery.post(self.ajaxhandler, {'action_url': url})
+        .done(function(data) {
+            self.actionsMenu.html(data);
+            var messages = self.actionsMenu.find(".portalMessage").detach();
+            var $kss_message = jQuery("#kssPortalMessage");
+            $kss_message.next(".portalMessage").remove();
+            $kss_message.after(messages);
+            window.initializeMenus();    //we need to reinitialize menus
+            // reinitilize async logic on new content actions
+            var async = new AsyncWorkflow();
+            async.initialize();
+            // trigger workflow menu refreshed events
+            jQuery(AsyncWorkflow.Events).trigger(AsyncWorkflow.Events.WORKFLOW_MENU_REFRESHED);
+        })
+        .fail(function() {
+            self.menuHeader.html("Failure!");
+        });
 };
 
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function() {
     var async = new AsyncWorkflow();
+    async.initialize();
 });
+
